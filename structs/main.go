6b0f61bc893d5ffd65a3f2b5aae28052dc6c3eb1
Marco <marcoarnone@easy-network.it>
2019-06-20 16:17:41 +0200

    create a new struct

--- structs/main.go
+++ structs/main.go
@@ -1,10 +1,36 @@
 package main
 
+import "fmt"
+
+type concactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	concact   concactInfo
 }
 
 func main() {
+	// alex := person{"Alex", "Anderson"}
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+	var alex person
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
 
+	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		concact: concactInfo{
+			email:   "jim.party@email.com",
+			zipCode: 9400,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
 }
