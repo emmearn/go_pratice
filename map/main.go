4caab2226d405fbb189a0a9c656c50e2a513354c
Marco <marcoarnone@easy-network.it>
2019-06-21 12:20:10 +0200

    maps

---
+++ map/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var colors map[string]string
+
+	// colors := make(map[string]string)
+
+	colors := map[string]string{
+		"red":   "#ff000",
+		"green": "#4bf45",
+	}
+
+	colors["white"] = "#fffff"
+
+	delete(colors, "green")
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
